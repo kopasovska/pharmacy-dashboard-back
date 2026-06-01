31ed4eeb7c3e87574c0a2d5b1a6522b7564039c9
kopasovska <v.kopasovska@gmail.com>
2026-06-01 22:53:42 +0200

    feat: implemented put request for suppliers

--- src/controllers/suppliersController.js
+++ src/controllers/suppliersController.js
@@ -1,3 +1,4 @@
+import createHttpError from 'http-errors';
 import { Supplier } from '../models/supplier.js';
 
 export const getSuppliers = async (req, res) => {
@@ -8,4 +9,20 @@ export const getSuppliers = async (req, res) => {
 export const addSupplier = async (req, res) => {
   const newSupplier = await Supplier.create(req.body);
   return res.status(201).json(newSupplier);
+};
+
+export const updateSupplier = async (req, res) => {
+  const { supplierId } = req.params;
+
+  const supplier = await Supplier.findOneAndUpdate(
+    { _id: supplierId },
+    req.body,
+    { returnDocument: 'after' },
+  );
+
+  if (!supplier) {
+    throw createHttpError(404, 'Supplier not found');
+  }
+
+  return res.status(200).json(supplier);
 };
--- src/routes/suppliersRoutes.js
+++ src/routes/suppliersRoutes.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import {
   addSupplier,
   getSuppliers,
+  updateSupplier,
 } from '../controllers/suppliersController.js';
 
 const router = new Router();
 
 router.get('/suppliers', getSuppliers);
 router.post('/suppliers', addSupplier);
+router.put('/suppliers/:supplierId', updateSupplier);
 
 export default router;
