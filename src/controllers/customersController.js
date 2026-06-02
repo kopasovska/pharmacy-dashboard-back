0154ba1b5c80fb0fe44c0797ef2202dbb729c193
kopasovska <v.kopasovska@gmail.com>
2026-06-02 23:00:40 +0200

    feat: add filtration by name for GET /customers request

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -1,11 +1,17 @@
 import { Customer } from '../models/customer.js';
 
 export const getCustomers = async (req, res, next) => {
-  const { page = 1, perPage = 10 } = req.query;
+  const { page = 1, perPage = 10, name } = req.query;
 
   const skip = (page - 1) * perPage;
 
   const customersQuery = Customer.find();
+
+  if (name) {
+    customersQuery.where({
+      name: { $regex: name, $options: 'i' },
+    });
+  }
 
   const [totalItems, customers] = await Promise.all([
     customersQuery.clone().countDocuments(),
--- src/validations/customersValidation.js
+++ src/validations/customersValidation.js
@@ -4,5 +4,6 @@ export const getCustomersSchema = {
   [Segments.PARAMS]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(50).default(10),
+    name: Joi.string().min(1).max(30),
   }),
 };
