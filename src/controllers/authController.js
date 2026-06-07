de38685168680f2bb610982aae6a4ca74c77de6d
kopasovska <v.kopasovska@gmail.com>
2026-06-07 23:18:33 +0200

    feat: create request-reset-email path with reset email template, implemented a password reset flow using JWT-based reset tokens, Nodemailer, and Brevo SMTP

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,12 @@
 import createHttpError from 'http-errors';
-import { User } from '../models/user.js';
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+import handlebars from 'handlebars';
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
+import { sendEmail } from '../utils/sendEmail.js';
+import { User } from '../models/user.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 import { Session } from '../models/session.js';
 
@@ -87,5 +93,48 @@ export const refreshUserSession = async (req, res) => {
 
   res.status(200).json({
     message: 'Session refreshed',
+  });
+};
+
+export const requestResetEmail = async (req, res) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(200).json({
+      message: 'If this email exists, a reset link has been sent',
+    });
+  }
+
+  const resetToken = jwt.sign(
+    { sub: user._id, email },
+    process.env.JWT_SECRET,
+    { expiresIn: '15m' },
+  );
+
+  const templatePath = path.resolve('src/templates/reset-password-email.html');
+  const templateSource = await fs.readFile(templatePath, 'utf-8');
+  const template = handlebars.compile(templateSource);
+  const html = template({
+    name: user.username,
+    link: `${process.env.FRONTEND_DOMAIN}/reset-password?token=${resetToken}`,
+  });
+
+  try {
+    await sendEmail({
+      from: process.env.SMTP_FROM,
+      to: email,
+      subject: 'Reset your password',
+      html,
+    });
+  } catch {
+    throw createHttpError(
+      500,
+      'Failed to send the email, please try again later.',
+    );
+  }
+
+  res.status(200).json({
+    message: 'If this email exists, a reset link has been sent',
   });
 };
--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -3,14 +3,20 @@ import { Joi, Segments } from 'celebrate';
 export const registerUserSchema = {
   [Segments.BODY]: Joi.object({
     username: Joi.string().min(3).max(30),
-    email: Joi.string().min(3).required(),
+    email: Joi.string().email().required(),
     password: Joi.string().min(8).required(),
   }),
 };
 
 export const loginUserSchema = {
   [Segments.BODY]: Joi.object({
-    email: Joi.string().min(3).required(),
+    email: Joi.string().email().required(),
     password: Joi.string().min(8).required(),
+  }),
+};
+
+export const requestResetEmailSchema = {
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().required(),
   }),
 };
