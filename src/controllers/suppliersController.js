02203b689afa29fcb8bb2fff89e7c4f81493b058
kopasovska <v.kopasovska@gmail.com>
2026-06-22 21:40:04 +0200

    refactor: move auth business logic to services (add suppliers services)

--- src/controllers/suppliersController.js
+++ src/controllers/suppliersController.js
@@ -1,53 +1,21 @@
-import createHttpError from 'http-errors';
-import { Supplier } from '../models/supplier.js';
 import {
-  calculatePaginationData,
-  parsePaginationParams,
-} from '../utils/pagination.js';
-import { parseSortParams } from '../utils/sort.js';
+  addSupplierService,
+  getSuppliersService,
+  updateSupplierService,
+} from '../services/suppliers.js';
 
 export const getSuppliers = async (req, res) => {
-  const { name } = req.query;
-
-  const { page, perPage, skip } = parsePaginationParams(req.query);
-  const { sortBy, sortOrder } = parseSortParams(req.query);
-
-  const suppliersQuery = Supplier.find();
-
-  if (name) {
-    suppliersQuery.where({ name: { $regex: name, $options: 'i' } });
-  }
-
-  const [totalItems, suppliers] = await Promise.all([
-    suppliersQuery.clone().countDocuments(),
-    suppliersQuery
-      .skip(skip)
-      .limit(perPage)
-      .sort({ [sortBy]: sortOrder }),
-  ]);
-
-  const paginationData = calculatePaginationData(totalItems, page, perPage);
-
-  return res.status(200).json({ ...paginationData, suppliers });
+  const data = await getSuppliersService(req.query);
+  return res.status(200).json(data);
 };
 
 export const addSupplier = async (req, res) => {
-  const newSupplier = await Supplier.create(req.body);
+  const newSupplier = await addSupplierService(req.body);
   return res.status(201).json(newSupplier);
 };
 
 export const updateSupplier = async (req, res) => {
   const { supplierId } = req.params;
-
-  const supplier = await Supplier.findOneAndUpdate(
-    { _id: supplierId },
-    req.body,
-    { returnDocument: 'after' },
-  );
-
-  if (!supplier) {
-    throw createHttpError(404, 'Supplier not found');
-  }
-
+  const supplier = await updateSupplierService(supplierId, req.body);
   return res.status(200).json(supplier);
 };
---
+++ src/services/suppliers.js
@@ -0,0 +1,38 @@
+import { Supplier } from '../models/supplier.js';
+import {
+  calculatePaginationData,
+  parsePaginationParams,
+} from '../utils/pagination.js';
+import { parseSortParams } from '../utils/sort.js';
+
+export const getSuppliersService = async (query) => {
+  const { name } = query;
+
+  const { page, perPage, skip } = parsePaginationParams(query);
+  const { sortBy, sortOrder } = parseSortParams(query);
+
+  const suppliersQuery = Supplier.find();
+
+  if (name) {
+    suppliersQuery.where({ name: { $regex: name, $options: 'i' } });
+  }
+
+  const [totalItems, suppliers] = await Promise.all([
+    suppliersQuery.clone().countDocuments(),
+    suppliersQuery
+      .skip(skip)
+      .limit(perPage)
+      .sort({ [sortBy]: sortOrder }),
+  ]);
+
+  const paginationData = calculatePaginationData(totalItems, page, perPage);
+
+  return { ...paginationData, suppliers };
+};
+
+export const addSupplierService = async (body) => {
+  const newSupplier = await Supplier.create(body);
+  return newSupplier;
+};
+
+export const updateSupplierService = async(supplierId, boddy);
